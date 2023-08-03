3f9b398d72e404546ee4a024f643ac0eae747b49
Timon Jurschitsch <timon.jurschitsch@gmail.com>
2023-08-03 19:21:05 +0200

    Improve gallery component, add modal

--- Components/PhotoGallery.tsx
+++ Components/PhotoGallery.tsx
@@ -1,116 +1,94 @@
 'use client';
 import { motion } from 'framer-motion';
 import Image from 'next/image';
+import { Dispatch, SetStateAction, useState } from 'react';
+
+export interface Photo {
+  url: string;
+  info?: {
+    aperture: string;
+    shutterSpeed: string;
+    focalLength: string;
+  };
+}
 
 export default function PhotoGallery() {
-  const imageUrlArray = [
-    { url: '/img/DSCF0187.jpg', aspectRatio: '3 / 2' },
-    { url: '/img/DSCF0385.jpg', aspectRatio: '3 / 2' },
-    { url: '/img/DSCF0431.jpg', aspectRatio: '3 / 2' },
-    { url: '/img/DSCF0480.jpg', aspectRatio: '3 / 2' },
-    { url: '/img/DSCF0560.jpg', aspectRatio: '3 / 2' },
-    { url: '/img/DSCF0602.jpg', aspectRatio: '3 / 2' },
+  const [selectedImage, setSelectedImage] = useState<Photo | undefined>();
+  const [isOpen, setIsOpen] = useState(false);
+  const imageUrlArray: Photo[] = [
+    { url: '/img/DSCF0187.jpg' },
+    { url: '/img/DSCF0385.jpg' },
+    { url: '/img/DSCF0431.jpg' },
+    { url: '/img/DSCF0480.jpg' },
+    { url: '/img/DSCF0560.jpg' },
+    { url: '/img/DSCF0602.jpg' },
   ];
 
   return (
-    <div className="flex justify-center items-center mt-24 p-12">
-      <div className="grid grid-cols-2 grid-flow-row gap-1 md:grid-cols-3 xl:grid-cols-4">
+    <div className="flex justify-center items-center mt-12 py-24 px-12">
+      <div className="grid grid-cols-2 grid-flow-row gap-x-5 gap-y-4 md:grid-cols-3 lg:grid-cols-4">
         {imageUrlArray.map((image) => {
           return (
-            <motion.div
-              key={image.url}
-              className={`p-2 aspect-square overflow-hidden`}
-              whileHover={{
-                scale: 1.5,
-                aspectRatio: image.aspectRatio,
-                width: 180 * parseInt(image.aspectRatio.split('/')[0]),
-                height: 180 * parseInt(image.aspectRatio.split('/')[1]),
-                transformOrigin: 'top left',
-                transitionDuration: '200ms',
-                zIndex: 10,
-              }}
-            >
+            <>
               <motion.div
-                className="w-full h-full rounded-lg overflow-hidden"
-                layout="size"
+                key={image.url}
+                className="aspect-square overflow-hidden w-[320px] h-[320px] hover:backdrop-brightness-75 hover:p-2 hover:max-h-[580px]"
+                whileHover={{
+                  scale: 2,
+                  transitionDuration: '100ms',
+                }}
               >
-                <Image
-                  alt="gallery"
-                  className="block h-full w-full rounded-lg object-cover object-center"
-                  width={1080}
-                  height={1080}
-                  src={image.url}
-                />
+                <div className="w-full h-full rounded-lg overflow-hidden">
+                  <Image
+                    alt="gallery"
+                    className="block h-full w-full rounded-lg object-cover object-center hover:object-contain"
+                    onClick={() => {
+                      setSelectedImage(image);
+                      setIsOpen(true);
+                    }}
+                    width={1080}
+                    height={1080}
+                    src={image.url}
+                  />
+                </div>
               </motion.div>
-            </motion.div>
+              <PhotoModal
+                image={selectedImage!}
+                isOpen={isOpen}
+                setIsOpen={setIsOpen}
+              />
+            </>
           );
         })}
       </div>
     </div>
   );
+}
 
-  // return (
-  //   <div className="container mx-auto px-5 py-2 lg:px-32 lg:pt-24">
-  //     <div className="-m-1 flex flex-wrap md:-m-2">
-  //       <div className="flex w-1/2 flex-wrap">
-  //         <div className="w-1/2 p-1 md:p-2">
-  //           <Image
-  //             alt="gallery"
-  //             className="block h-full w-full rounded-lg object-cover object-center"
-  //             width={480}
-  //             height={480}
-  //             src={imageUrlArray[0]}
-  //           />
-  //         </div>
-  //         <div className="w-1/2 p-1 md:p-2">
-  //           <Image
-  //             alt="gallery"
-  //             className="block h-full w-full rounded-lg object-cover object-center"
-  //             height={480}
-  //             width={480}
-  //             src={imageUrlArray[1]}
-  //           />
-  //         </div>
-  //         <div className="w-full p-1 md:p-2">
-  //           <Image
-  //             alt="gallery"
-  //             className="block h-full w-full rounded-lg object-cover object-center"
-  //             width={480}
-  //             height={480}
-  //             src={imageUrlArray[2]}
-  //           />
-  //         </div>
-  //       </div>
-  //       <div className="flex w-1/2 flex-wrap">
-  //         <div className="w-full p-1 md:p-2">
-  //           <Image
-  //             alt="gallery"
-  //             className="block h-full w-full rounded-lg object-cover object-center"
-  //             height={480}
-  //             width={480}
-  //             src={imageUrlArray[3]}
-  //           />
-  //         </div>
-  //         <div className="w-1/2 p-1 md:p-2">
-  //           <Image
-  //             alt="gallery"
-  //             className="block h-full w-full rounded-lg object-cover object-center"
-  //             width={480}
-  //             height={480}
-  //             src={imageUrlArray[4]}
-  //           />
-  //         </div>
-  //         <div className="w-1/2 p-1 md:p-2">
-  //           <Image
-  //             alt="gallery"
-  //             className="block h-full w-full rounded-lg object-cover object-center"
-  //             height={480}
-  //             width={480}
-  //             src={imageUrlArray[5]}
-  //           />
-  //         </div>
-  //       </div>
-  //     </div>
-  //   </div>
-  // );
+interface PhotoModalProps {
+  image: Photo;
+  isOpen: boolean;
+  setIsOpen: Dispatch<SetStateAction<boolean>>;
+}
+function PhotoModal({ isOpen, setIsOpen, image }: PhotoModalProps) {
+  return (
+    <dialog open={isOpen} className="modal">
+      <form method="dialog" className="modal-box">
+        <Image
+          src={image.url}
+          alt=""
+          width={1080}
+          height={1080}
+          className="object-contain"
+        />
+        <p className="py-4">Click the button below to close</p>
+        <div className="modal-action">
+          {/* if there is a button in form, it will close the modal */}
+          <button className="btn" onClick={() => setIsOpen(false)}>
+            Close
+          </button>
+        </div>
+      </form>
+    </dialog>
+  );
 }
